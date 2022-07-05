b26691b06af8ece4d40ec40058c27c6faf4d9ed3
Vera Ho <vh71886@gmail.com>
2022-07-04 21:34:11 -0400

    Success

---
+++ README.md
@@ -0,0 +1,7 @@
+# Setup
+1. npm init -y or npm init (to enter parameters)
+2. npm install express mongoose passport passport-jwt jsonwebtoken body-parser bcryptjs validator 
+3. npm install -D nodemon
+4. add `"type": "module"` to package.json for using ES6
+5. check server runs using `node index.js`
+6. npm install aws-sdk
--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 import express from 'express';
+import AWS from 'aws-sdk';
+import keys from './config/keys.js'
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+
 app.get('/', (req, res) => {
-    res.send("Hello World!");
+    AWS.config.update(keys);
+    let s3 = new AWS.S3();
+
+    async function getImage(){
+        const data = s3.getObject({
+            Bucket: 'dina-dopt-dev',
+            Key: 'velociraptor-paleorex-full.jpeg'
+            })
+            .promise();
+        console.log(data);
+        return data;
+    }
+
+    const encode = data => {
+        let buf = Buffer.from(data);
+        let base64 = buf.toString('base64');
+        return base64;
+    }
+
+    getImage().then( img =>  {
+        console.log(img);
+        let image="<img src='data:image/jpeg;base64," + encode(img.Body) + "'" + "/>";
+        let startHTML = "<html><body></body>";
+        let endHTML = "</body></html>";
+        let html = startHTML + image + endHTML;
+        res.send(html)
+        }).catch( (e) => {
+            res.send(e)
+    })
 });
 
 app.listen(PORT, () => {
